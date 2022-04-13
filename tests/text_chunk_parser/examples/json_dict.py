fedcd655869a214202d8618f08055a27f9761023
Chad Lowe <pfmsoft.dev@gmail.com>
2022-04-12 18:14:31 -0700

    json dict example with tests

--- tests/text_chunk_parser/examples/json_dict.py
+++ tests/text_chunk_parser/examples/json_dict.py
@@ -1,7 +1,8 @@
 import re
-from typing import Dict, Tuple
+from typing import Dict, Sequence, Tuple
 
-from pfmsoft.text_chunk_parser import Chunk, ChunkParser, ParseContext
+from pfmsoft.text_chunk_parser import Chunk, ChunkParser, ParseContext, ParseSchema
+from pfmsoft.text_chunk_parser.text_chunk_parser import EmptyLine
 
 JSON_DICT = """
 foo = {
@@ -16,13 +17,126 @@ foo = {
     ],
 }
 """
+# TODO option to not advance state
+class JsonParseSchema(ParseSchema):
+    def __init__(self):
+        self.schema = {
+            "origin": [EmptyLine(), IdentifierLine()],
+            "empty_line": [IdentifierLine(), EmptyLine()],
+            "identifier": [KeyValueLine()],
+            "key_value": [KeyValueLine(), KeyListLine()],
+            "key_list": [ListValueLine(), ListEndLine()],
+            "list_value": [ListValueLine(), ListEndLine()],
+            "list_end": [DictEndLine()],
+            "dict_end": [EmptyLine()],
+        }
+
+    def expected(self, state: str) -> Sequence[ChunkParser]:
+        parsers = self.schema.get(state, None)
+        if parsers is None:
+            raise NotImplementedError()
+        return parsers
+
+
+class JsonParseContext(ParseContext):
+    def parsed_data(self, chunk_parser: "ChunkParser", state, data):
+        """Handle the parsed data."""
+        pass
+
+    def initialize(self):
+        """
+        Do any work required to initialize the context.
+
+        _extended_summary_
+        """
+
+    def cleanup(self):
+        """
+        Do any work required to clean up after context
+
+        """
+
+
+class DictEndLine(ChunkParser):
+    def parse(
+        self,
+        chunk: Chunk,
+        state: str,
+        context: ParseContext,
+    ) -> Tuple[str, Dict]:
+        regex = r"^}\n$"
+        pattern = re.compile(regex)
+        match = pattern.match(chunk.text)
+        if match:
+            return (
+                "dict_end",
+                {},
+            )
+        return self.report_parse_fail(chunk, state)
+
+
+class ListEndLine(ChunkParser):
+    def parse(
+        self,
+        chunk: Chunk,
+        state: str,
+        context: ParseContext,
+    ) -> Tuple[str, Dict]:
+        regex = r"^\s*\],\n$"
+        pattern = re.compile(regex)
+        match = pattern.match(chunk.text)
+        if match:
+            return (
+                "list_end",
+                {},
+            )
+        return self.report_parse_fail(chunk, state)
+
+
+class ListValueLine(ChunkParser):
+    def parse(
+        self,
+        chunk: Chunk,
+        state: str,
+        context: ParseContext,
+    ) -> Tuple[str, Dict]:
+        regex = r"^\s*\"(?P<value>[\w\s'`\-]+)\",\n$"
+        pattern = re.compile(regex)
+        match = pattern.match(chunk.text)
+        if match:
+            return (
+                "list_value",
+                {"value": match.group("value")},
+            )
+        return self.report_parse_fail(chunk, state)
+
+
+class KeyListLine(ChunkParser):
+    def parse(
+        self,
+        chunk: Chunk,
+        state: str,
+        context: ParseContext,
+    ) -> Tuple[str, Dict]:
+        regex = r"^\s*\"(?P<key>[\w\s]+)\"\:\s*(?P<value>\[)\n$"
+        pattern = re.compile(regex)
+        match = pattern.match(chunk.text)
+        if match:
+            return (
+                "key_list",
+                {"key": match.group("key")},
+            )
+        return self.report_parse_fail(chunk, state)
 
 
 class KeyValueLine(ChunkParser):
     def parse(
-        self, chunk: Chunk, state: str, context: ParseContext
+        self,
+        chunk: Chunk,
+        state: str,
+        context: ParseContext,
     ) -> Tuple[str, Dict]:
-        regex = r"\s*\"(?P<key>[\w\s]+)\"\:\s*\"(?P<value>[\w\s.]+)\",\n"
+        regex = r"^\s*\"(?P<key>[\w\s]+)\"\:\s*\"(?P<value>[\w\s.]+)\",\n$"
         pattern = re.compile(regex)
         match = pattern.match(chunk.text)
         if match:
@@ -35,32 +149,17 @@ class KeyValueLine(ChunkParser):
 
 class IdentifierLine(ChunkParser):
     def parse(
-        self, chunk: Chunk, state: str, context: ParseContext
+        self,
+        chunk: Chunk,
+        state: str,
+        context: ParseContext,
     ) -> Tuple[str, Dict]:
         tokens = chunk.text.split()
-        print(chunk)
         if (
             len(tokens) == 3
             and tokens[0].isidentifier()
             and tokens[1] == "="
             and tokens[2] == "{"
         ):
-            return ("identifier", {"identifier": tokens[0], "terminus": tokens[2]})
+            return ("identifier", {"identifier": tokens[0]})
         return self.report_parse_fail(chunk, state)
-
-
-class EmptyLine(ChunkParser):
-    def parse(
-        self, chunk: Chunk, state: str, context: ParseContext
-    ) -> Tuple[str, Dict]:
-        regex = r"(?P<whitespace>[^\S\n]*)\n"
-        pattern = re.compile(regex)
-        match = pattern.match(chunk.text)
-        if match:
-            return ("empty_line", {"whitespace": match.group("whitespace")})
-        return self.report_parse_fail(chunk, state)
-
-
-def parse_schema():
-    schema = {"origin": [IdentifierLine]}
-    return schema
--- tests/text_chunk_parser/test_json_dict.py
+++ tests/text_chunk_parser/test_json_dict.py
@@ -1,63 +1,158 @@
+import logging
+from dataclasses import dataclass, field
+from pickle import FALSE
+from typing import Dict, Sequence
+
 import pytest
 from tests.text_chunk_parser.examples.json_dict import (
     JSON_DICT,
-    EmptyLine,
+    DictEndLine,
     IdentifierLine,
+    JsonParseContext,
+    JsonParseSchema,
+    KeyListLine,
     KeyValueLine,
+    ListEndLine,
+    ListValueLine,
 )
 
-from pfmsoft.text_chunk_parser import ParseContext, Parser, StringChunkProvider
-from pfmsoft.text_chunk_parser.text_chunk_parser import Chunk, FailedParseException
+from pfmsoft.text_chunk_parser import (
+    Chunk,
+    EmptyLine,
+    FailedParseException,
+    Parser,
+    StringChunkProvider,
+)
+from pfmsoft.text_chunk_parser.text_chunk_parser import (
+    AllFailedToParseException,
+    ChunkParser,
+)
+
+
+@dataclass
+class ParseTest:
+    text: str
+    state: str
+    data: Dict = field(default_factory=dict)
+
+
+def test_parse_schema(caplog):
+    caplog.set_level(logging.INFO)
+    context = JsonParseContext()
+    schema = JsonParseSchema()
+    parser = Parser(schema, log_on_success=True)
+    with StringChunkProvider(JSON_DICT) as provider:
+        try:
+            parser.parse(context, provider)
+        except AllFailedToParseException as exc:
+            print(exc)
+            assert False
+
+
+def parse_test(parse_tests: Sequence[ParseTest], parser: ChunkParser):
+    for count, test in enumerate(parse_tests):
+        chunk = Chunk(str(count), test.text)
+        state, data = parser().parse(chunk, "origin", None)
+        assert state == test.state
+        assert data == test.data
+
+
+def parse_test_fail(parse_tests: Sequence[ParseTest], parser: ChunkParser):
+    for count, test in enumerate(parse_tests):
+        chunk = Chunk(str(count), test.text)
+        with pytest.raises(FailedParseException) as exc:
+            _, _ = parser().parse(chunk, "origin", None)
+            assert exc.state == "origin"  # type: ignore
+            assert exc.parser is FailedParseException  # type: ignore
+            assert exc.chunk == chunk  # type: ignore
 
 
 def test_identier_parse():
-    with StringChunkProvider(JSON_DICT) as provider:
-        chunk = next(provider)
-        chunk = next(provider)
-        state, data = IdentifierLine().parse(chunk, "origin", None)
-    assert state == "identifier"
-    assert data == {"identifier": "foo", "terminus": "{"}
+    parse_tests: Sequence[ParseTest] = [
+        ParseTest("foo = {\n", "identifier", {"identifier": "foo"})
+    ]
+    parse_test(parse_tests, IdentifierLine)
 
 
 def test_identifier_parse_fail():
-    with StringChunkProvider(JSON_DICT) as provider:
-        chunk = next(provider)
-        with pytest.raises(FailedParseException) as exc:
-            _, _ = IdentifierLine().parse(chunk, "origin", None)
-            assert exc.state == "origin"
-            assert exc.parser is FailedParseException
-            assert exc.chunk == chunk
+    parse_tests: Sequence[ParseTest] = [ParseTest("  bad value  ],\n", "list_end", {})]
+    parse_test_fail(parse_tests, IdentifierLine)
 
 
 def test_empty_line_parse():
-    with StringChunkProvider(JSON_DICT) as provider:
-        chunk = next(provider)
-        state, data = EmptyLine().parse(chunk, "origin", None)
-    assert state == "empty_line"
-    assert data == {"whitespace": ""}
-
-    chunk = Chunk("1", "   \n")
-    state, data = EmptyLine().parse(chunk, "origin", None)
-    assert state == "empty_line"
-    assert data == {"whitespace": "   "}
+    parse_tests: Sequence[ParseTest] = [
+        ParseTest("\n", "empty_line", {"whitespace": ""}),
+        ParseTest("   \n", "empty_line", {"whitespace": "   "}),
+    ]
+    parse_test(parse_tests, EmptyLine)
 
 
 def test_empty_line_parse_fail():
-    with StringChunkProvider(JSON_DICT) as provider:
-        chunk = next(provider)
-        chunk = next(provider)
-        with pytest.raises(FailedParseException) as exc:
-            _, _ = EmptyLine().parse(chunk, "origin", None)
-            assert exc.state == "origin"
-            assert exc.parser is FailedParseException
-            assert exc.chunk == chunk
+    parse_tests: Sequence[ParseTest] = [ParseTest("  bad value  ],\n", "list_end", {})]
+    parse_test_fail(parse_tests, EmptyLine)
 
 
 def test_key_value_line():
-    with StringChunkProvider(JSON_DICT) as provider:
-        chunk = next(provider)
-        chunk = next(provider)
-        chunk = next(provider)
-        state, data = KeyValueLine().parse(chunk, "origin", None)
-    assert state == "key_value"
-    assert data == {"key": "Name", "value": "B. S. Johnson"}
+    parse_tests: Sequence[ParseTest] = [
+        ParseTest(
+            '    "Name": "B. S. Johnson",\n',
+            "key_value",
+            {"key": "Name", "value": "B. S. Johnson"},
+        )
+    ]
+    parse_test(parse_tests, KeyValueLine)
+
+
+def test_key_value_line_parse_fail():
+    parse_tests: Sequence[ParseTest] = [ParseTest("  bad value  ],\n", "list_end", {})]
+    parse_test_fail(parse_tests, KeyValueLine)
+
+
+def test_key_list_line():
+    parse_tests: Sequence[ParseTest] = [
+        ParseTest(
+            '    "Notable Creations": [\n', "key_list", {"key": "Notable Creations"}
+        )
+    ]
+    parse_test(parse_tests, KeyListLine)
+
+
+def test_key_list_line_parse_fail():
+    parse_tests: Sequence[ParseTest] = [ParseTest("  bad value  ],\n", "list_end", {})]
+    parse_test_fail(parse_tests, KeyListLine)
+
+
+def test_list_value_line():
+    parse_tests: Sequence[ParseTest] = [
+        ParseTest(
+            """        "Archchancellor's Bathroom",\n""",
+            "list_value",
+            {"value": "Archchancellor's Bathroom"},
+        )
+    ]
+    parse_test(parse_tests, ListValueLine)
+
+
+def test_list_value_parse_fail():
+    parse_tests: Sequence[ParseTest] = [ParseTest("  bad value  ],\n", "list_end", {})]
+    parse_test_fail(parse_tests, ListValueLine)
+
+
+def test_list_end_line():
+    parse_tests: Sequence[ParseTest] = [ParseTest("    ],\n", "list_end", {})]
+    parse_test(parse_tests, ListEndLine)
+
+
+def test_list_end_parse_fail():
+    parse_tests: Sequence[ParseTest] = [ParseTest("  bad value  ],\n", "list_end", {})]
+    parse_test_fail(parse_tests, ListEndLine)
+
+
+def test_dict_end_line():
+    parse_tests: Sequence[ParseTest] = [ParseTest("}\n", "dict_end", {})]
+    parse_test(parse_tests, DictEndLine)
+
+
+def test_dict_end_parse_fail():
+    parse_tests: Sequence[ParseTest] = [ParseTest("  bad value  ],\n", "list_end", {})]
+    parse_test_fail(parse_tests, DictEndLine)
